bf4c01ac3727655a940599c66df19dfbb1be4bd6
Simon Voorhees <sxv2721@g.rit.edu>
2016-11-21 03:03:29 -0500

    Added enemies, basic enemy and bullet sprites, made enemies track players(looks off), flash yellow, and shoot, Added an enemy handler object. If the player gets hit currently nothing happens

---
+++ ProjectSheathe/Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    // Use this for initialization
+    GameObject Player;
+    GameObject Bullet;
+    Vector3 velocity = new Vector3(0, 0, 0);
+    Vector3 vecToPlayer = new Vector3(0, 0, 0);
+    bool firing = false;
+    int timer = 0;
+    bool active = false;
+    System.Random rand = new System.Random();
+	void Start () {
+        Bullet = new GameObject();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Bullet.AddComponent<SpriteRenderer>();
+        Bullet.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Bullet");
+        Bullet.AddComponent<BoxCollider2D>();
+        Bullet.transform.SetParent(this.transform);
+        Bullet.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            Bullet.transform.position += velocity;
+            if (Bullet.GetComponent<BoxCollider2D>().IsTouching(Player.GetComponent<BoxCollider2D>()))
+            {
+                Bullet.SetActive(false);
+                active = false;
+                //Bullet.GetComponent<SpriteRenderer>
+            }
+        }/**/
+        ++timer;
+        if (!firing)
+        {//tracks the player
+            vecToPlayer = (Player.transform.position - this.transform.position);
+            float angle = Mathf.Atan2(vecToPlayer.y, vecToPlayer.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 0.9f); //Quaternion.LookRotation(this.transform.position - Player.transform.position);
+            if(timer>600)
+            {
+                firing = true;
+                timer = rand.Next(0,300);
+            }
+        }
+        else
+        {
+            if (timer % 3 == 0)
+            {
+                this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 0);
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+            }
+            if(timer >=100)
+            {
+                Bullet.SetActive(true);
+                active = true;
+                this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+                velocity = (Player.transform.position - this.transform.position).normalized;
+                Bullet.transform.position = this.transform.position;
+                firing = false;
+            }
+        }
+    }
+}
---
+++ ProjectSheathe/Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyHandler : MonoBehaviour {
+
+    List<GameObject> Enemies = new List<GameObject>();
+    // Use this for initialization
+    System.Random rand = new System.Random();
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Enemies.Count<5)
+        {
+            CreateEnemy();
+        }
+        
+	}
+
+    void CreateEnemy()
+    {
+        GameObject E = new GameObject();
+        E.AddComponent<SpriteRenderer>();
+        E.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Enemy");
+        E.transform.SetParent(this.transform);
+        E.transform.Translate(new Vector2(rand.Next(-3, 3), 2+rand.Next(-3, 3)));
+        E.AddComponent<Enemy>();
+        E.AddComponent<BoxCollider2D>();
+        Enemies.Add(E);
+    }
+}
